503d5a7dfa5a265fc03247c487e389355e12c8f4
byoukinn <1209418977@qq.com>
2019-09-12 18:55:59 +0800

    add devserver proxy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
     publicPath: '.',
+    devServer: {
+        overlay: {
+            warnings: false,
+            errors: true,
+        },
+        disableHostCheck: true,
+        proxy: {
+            '/api': {
+                target: 'http://blog-admin.zhangbk.cn/api/public/',
+                ws: false,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '',
+                }
+            },
+        }
+    }
 }
