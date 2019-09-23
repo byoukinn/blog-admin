b7598a95c66599fe82e4eba478b56159f5b76d89
byoukinn <1209418977@qq.com>
2019-09-23 20:21:54 +0800

    update article list

--- src/views/article/ArticleList.vue
+++ src/views/article/ArticleList.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<el-table :data="tableData" border style="width: 100%" v-loading="loading">
+		<el-table :data="tableData" style="width: 100%" v-loading="loading">
 			<el-table-column prop="cover" label="封面" width="222">
 				<template slot-scope="scope">
 					<img width="200" :src="scope.row.cover" :alt="scope.row.title" />
@@ -10,10 +10,10 @@
 			<el-table-column prop="title" label="标题" width="200"></el-table-column>
 			<el-table-column prop="status" label="状态" width="60"></el-table-column>
 			<el-table-column prop="update_time" label="最后更新时间" width="180"></el-table-column>
-			<el-table-column prop="desc" label="描述" width="300"></el-table-column>
+			<el-table-column prop="desc" label="描述"></el-table-column>
 			<el-table-column fixed="right" label="操作" width="100">
 				<template slot-scope="scope">
-					<el-button  type="text" size="small">查看</el-button>
+					<el-button type="text" size="small">查看</el-button>
 					<el-button type="text" size="small" @click="editRow(scope.row)">编辑</el-button>
 				</template>
 			</el-table-column>
@@ -25,6 +25,7 @@
 import Article from '../../services/models/article'
 import ArticleInfo from './ArticleInfo'
 export default {
+<<<<<<< HEAD
 	data() {
 		return {
 			tableData: [],
@@ -87,6 +88,65 @@ export default {
 			});
 		}
 	},
+=======
+  data() {
+    return {
+      tableData: [],
+      loading: false,
+      page: 1,
+      rowSize: 10,
+      filter: '',
+    }
+  },
+  created() {
+    this.setData()
+  },
+  components: {
+    ArticleInfo,
+  },
+  methods: {
+    async setData() {
+      this.loading = true
+      let res = await Article.getArticles(this.page, this.rowSize)
+      this.loading = false
+      this.tableData = res.data.data
+      // if (res.code == 200) {
+      //   this.tableData = res.data
+      // } else {
+      //   this.$message.error(res.result)
+      // }
+    },
+    async editRow(data) {
+      const h = this.$createElement;
+      this.$msgbox({
+        title: '编辑文章',
+        message: h('ArticleInfo'),
+        showCancelButton: true,
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        beforeClose: (action, instance, done) => {
+          if (action === 'confirm') {
+            instance.confirmButtonLoading = true;
+            instance.confirmButtonText = '执行中...';
+            setTimeout(() => {
+              done();
+              setTimeout(() => {
+                instance.confirmButtonLoading = false;
+              }, 300);
+            }, 3000);
+          } else {
+            done();
+          }
+        }
+      }).then(action => {
+        this.$message({
+          type: 'info',
+          message: 'action: ' + action
+        });
+      });
+    }
+  },
+>>>>>>> update article list
 }
 </script>
 
